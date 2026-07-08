eee034712d32ea898e38f52fff56748fe9e332f5
Piotr Czajka <88501046+Beyshin@users.noreply.github.com>
2026-07-09 01:02:50 +0200

    Add: dijkstra - submission-1

---
+++ Data Structures & Algorithms/dijkstra/submission-1.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    unordered_map<int, int> shortestPath(int n, vector<vector<int>>& edges, int src) {
+       vector<vector<pair<int,int>>> adj(n);
+       unordered_map<int, int> distances;
+
+        for(int i = 0; i<n; i++){
+            distances[i] = INT_MAX;
+        }
+        distances[src] = 0;
+
+        for(const auto& e : edges){
+            adj[e[0]].push_back({e[1], e[2]});
+        };
+
+        priority_queue<pair<int, int>, vector<pair<int,int>>, greater<pair<int, int>>> pq;
+        pq.push({0, src});
+
+        while(!pq.empty()){
+            auto [curr_dist, u] = pq.top();
+            pq.pop();
+
+            if(curr_dist > distances[u]) continue;
+
+            for(auto& neighbor : adj[u]){
+                int v = neighbor.first;
+                int distance_to = neighbor.second;
+                if(distances[u] + distance_to < distances[v]){
+                    distances[v] = distances[u] + distance_to;
+                    pq.push({distances[v], v});
+                }
+            }
+        }
+
+        for(int i = 0; i < n; i++){
+            if(distances[i] == INT_MAX) distances[i] = -1;
+        }
+
+        return distances;
+    }
+};
